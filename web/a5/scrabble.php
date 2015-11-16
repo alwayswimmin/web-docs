e43c9b02dcd118ea9d44668962bd783596a42444
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2015-11-16 07:52:27 -0500

    changed output scrabble

--- web/a5/scrabble.php
+++ web/a5/scrabble.php
@@ -1,8 +1,20 @@
 <?php
-$pattern=$_REQUEST["regex"];
-$contents=file_get_contents("words.txt");
+$pattern=$_REQUEST['regex'];
+$contents=file_get_contents('words.txt');
 if(preg_match_all( '/' . $pattern . '/m', $contents, $matches)) {
-	echo json_encode(implode("<br>", $matches[0]));
+	usort($matches[0],'sort');
+	$length = 0;
+	foreach($matches[0] as $key => $val) {
+		if(strlen($val) == $length) {
+			echo json_encode('<br>' . $val);
+		} else {
+			$length = strlen($val);
+			echo json_encode('<h4>Strings of Length ' . $length . '</h4>' . $val);
+		}
+	}
 }
 exit;
+function sort($a,$b){
+    return strlen($b)-strlen($a);
+}
 ?>
