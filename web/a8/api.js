2b26df97043e30f4551ff4c25a396e8e69d892a4
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2016-01-07 14:22:16 -0500

    fixed api

--- web/a8/api.js
+++ web/a8/api.js
@@ -1,7 +1,7 @@
 $("#form").submit(function(e){
 	var station = $("#stat").val();
 	$.getJSON("http://api.usno.navy.mil/rstt/oneday?date=today&loc=" + station, function(data){
-		var rise = (data.sundata[2]).time;
+		var rise = (data.sundata[1]).time;
 		var set = (data.sundata[4]).time;
 		$("#rise").text(rise);
 		$("#set").text(set);
