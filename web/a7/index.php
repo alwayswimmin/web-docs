a9a12246973aaf5635379ef2e4c2a2d3ae79b4cb
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2016-01-07 13:47:34 -0500

    added api, finished form

--- web/a7/index.php
+++ web/a7/index.php
@@ -1,28 +1,12 @@
 <html>
 <head>
-	<title>Keep Count</title>
-	<style type = "text/css">
-		div {
-			background-color:green;
-		}
-	</style>
+	<title>Sun and Moon</title>
 </head>
 <body>
-	<?php
-		if (file_exists('time.txt')) {
-			$time = file_get_contents("time.txt");
-			echo "<div>Last visit at $time.</div>";
-		}
-		$time = date(DATE_RSS);
-		file_put_contents('time.txt', $time . "\n");
-		$filename = "visit_count.txt";
-		if(file_exists($filename)) {
-			$count = intval(file_get_contents($filename)) + 1;
-		} else {
-			$count = 1;
-		}
-		echo "<div>This page has been visited $count time" . ($count != 1 ? "s" : "") . ".</div>";
-		file_put_contents($filename, $count);
-	?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script type="text/javascript">
+		var url = "http://api.usno.navy.mil/rstt/oneday?date=today&loc=Washington,%20DC";
+		var data = jQuery.getJSON(url);
+	</script>
 </body>
 </html>
